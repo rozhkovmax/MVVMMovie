b4055ce8d86df7feb1a52fb646353a378f5f038b
Maxim Rozhkov <rojkov.m@rambler.ru>
2023-02-08 19:37:43 +0300

    feature initial commit

--- UIKitMovie/UIKitMovie/AppDelegate.swift
+++ UIKitMovie/UIKitMovie/AppDelegate.swift
@@ -7,7 +7,7 @@ import UIKit
 @main
 final class AppDelegate: UIResponder, UIApplicationDelegate {
     // MARK: - Public Methods
-
+    
     func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [
